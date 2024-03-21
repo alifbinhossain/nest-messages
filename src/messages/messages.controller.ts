c94ebc8f1d0860aa9bb16e65476f2cfc5fa74b67
alifbinhossain <alifahmed.47@gmail.com>
2024-03-22 05:26:56 +0600

    ci: :sparkles: inject service into controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,27 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  @Get()
-  listMessages() {
-    return 'list message';
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
   }
 
-  @Post()
-  createMessage(@Body() body: CreateMessageDto) {
-    return body;
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
   }
 
   @Get('/:id')
   getSingleMessage(@Param('id') id: string) {
-    return id;
+    return this.messagesService.findOne(id);
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
   }
 }
