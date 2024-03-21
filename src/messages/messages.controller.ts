060917a2e6e737e913d6ea94ae1bf77e2038de58
alifbinhossain <alifahmed.47@gmail.com>
2024-03-22 05:45:36 +0600

    ci: :triangular_flag_on_post: handled not found exception

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -16,8 +23,14 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getSingleMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getSingleMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+
+    return message;
   }
 
   @Post()
